09727b7ac86acc97a6ff4882603285243ab4aa83
sixj <sixj@runlion.com>
2020-08-14 20:12:24 +0800

    add: 多种语言标识

--- src/main/java/com/runlion/multilingual/translater/TranslaterDemo.java
+++ src/main/java/com/runlion/multilingual/translater/TranslaterDemo.java
@@ -10,7 +10,36 @@ public class TranslaterDemo {
         GoogleApi googleApi = new GoogleApi();
         // 通过代理
 //        GoogleApi googleApi = new GoogleApi("122.224.227.202", 3128);
-        String result = googleApi.translate("hello world",  "zh");
-        System.out.println(result);
+        // 英语
+        String s1 = googleApi.translate("你好",  "en");
+        // 印尼语
+        String s2 = googleApi.translate("你好",  "id");
+        // 德语
+        String s3 = googleApi.translate("你好",  "de");
+        // 法语
+        String s4 = googleApi.translate("你好",  "fr");
+        // 俄语
+        String s5 = googleApi.translate("你好",  "ru");
+        // 韩语
+        String s6 = googleApi.translate("你好",  "ko");
+        // 日语
+        String s7 = googleApi.translate("你好",  "ja");
+        // 泰语
+        String s8 = googleApi.translate("你好",  "th");
+        // 老挝语
+        String s9 = googleApi.translate("你好",  "lo");
+
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+        System.out.println(s7);
+        System.out.println(s8);
+        System.out.println(s9);
+
+
     }
 }
